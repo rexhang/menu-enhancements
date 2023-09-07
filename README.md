17ae6fd93a7487306327af68cb92b14a2ef09f21
rexhang(hang.gu | 顾航) <rexhang_work@outlook.com>
2023-09-07 10:16:51 +0800

    feature(add feature): copy file path.
    Signed-off-by: rexhang(hang.gu | 顾航) <rexhang_work@outlook.com>

---
+++ README.md
@@ -0,0 +1,23 @@
+# display-the-file-in-explorer
+
+这是一个不错的扩展，可以在你打开的任何文件内容区域内右键 直接打开所在的磁盘目录，并且高亮显示。
+
+> 如何开发
+
+```bash
+npm i -g yo generator-code vsce
+```
+
+初始化选择 `ts` & `yarn`
+
+#### dev
+```bash
+yarn install
+```
+
+`F5` 进行实例化调试，`ctrl+shift+i` 打开 **devtools**
+
+### build
+```bash
+npm run build
+```
---
+++ src/extension.ts
@@ -0,0 +1,78 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import { exec } from 'child_process';
+
+export function activate(context: vscode.ExtensionContext) {
+	// 注册命令
+	const displayTheFileInExplorer = vscode.commands.registerCommand('extension.displayTheFileInExplorer', () => {
+		// 获取当前活动编辑器的文件URI
+		const activeEditor = vscode.window.activeTextEditor;
+		if (!activeEditor) {
+			vscode.window.showErrorMessage('没有活动的编辑器。');
+			return;
+		}
+
+		// 获取当前编辑器的文件路径
+		const filePath = activeEditor.document.uri.fsPath;
+
+		// 获取文件所在的目录路径
+		const fileDir = path.dirname(filePath);
+
+		// 提示文件路径
+		vscode.window.showInformationMessage(`已在资源管理器中打开 ${filePath} 所在目录并且选中`);
+
+		console.log(`当前文件路径: ${filePath}`);
+
+		// 打开资源管理器并选中文件所在的目录
+		openFileExplorer(fileDir, filePath);
+	});
+
+	const copyRelativePath = vscode.commands.registerCommand('extension.copyRelativePath', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      const filePath = editor.document.uri.fsPath;
+      const workspaceFolder = vscode.workspace.getWorkspaceFolder(editor.document.uri);
+      if (workspaceFolder) {
+        const relativePath = path.relative(workspaceFolder.uri.fsPath, filePath);
+        vscode.env.clipboard.writeText(relativePath);
+        vscode.window.showInformationMessage(`已复制相对路径到剪贴板: ${relativePath}`);
+      } else {
+        vscode.window.showErrorMessage('无法确定工作区文件夹。');
+      }
+    } else {
+      vscode.window.showErrorMessage('没有活动的编辑器。');
+    }
+  });
+
+	const copyAbsolutePath = vscode.commands.registerCommand('extension.copyAbsolutePath', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      const filePath = editor.document.uri.fsPath;
+      vscode.env.clipboard.writeText(filePath);
+      vscode.window.showInformationMessage(`已复制绝对路径到剪贴板: ${filePath}`);
+    } else {
+      vscode.window.showErrorMessage('没有活动的编辑器。');
+    }
+  });
+
+	context.subscriptions.push(displayTheFileInExplorer, copyRelativePath, copyAbsolutePath);
+}
+
+function openFileExplorer(directoryPath: string, filePath: string) {
+	if (process.platform === 'win32') {
+		// Windows系统下使用explorer命令并选中文件
+		const command = `explorer /select,"${filePath}"`;
+
+		exec(command, (err) => {
+			if (err) {
+				// 由于 Windows 上的 explorer /select 命令不提供返回结果，exec 函数会将错误信息输出到控制台。
+				// 这就是为什么在控制台中会看到 ERR [Extension Host] Error: Command failed: ... 的错误信息。
+				// 这个错误信息并不影响资源目录的打开，只是表明在执行命令时可能遇到了某些问题，但并不影响整体功能的实现。
+				console.warn(err, '>>>>>>此错误并不影响逻辑功能，可以忽略。<<<<<<');
+			}
+		});
+	} else {
+		// 非Windows系统使用vscode.commands.executeCommand('revealInOS')方法
+		vscode.commands.executeCommand('revealInOS', vscode.Uri.file(filePath));
+	}
+}
